5e3b20c0e2eb47664bea8a41595a40658ee87f75
Kristofer Knutsen <Knutsen@KrisMac.local>
2019-11-29 08:54:52 +0200

    Merge issues

--- Assets/Scripts/Cards/CardManager.cs
+++ Assets/Scripts/Cards/CardManager.cs
@@ -8,6 +8,15 @@ public class CardManager : MonoBehaviour
     //Card manager singleton
     public static CardManager instance;
     public Text scoreDisplay;
+
+    public Canvas cardCanvas;
+    public GameObject tutorialScreen;
+    public GameObject correctImage, failImage;
+    private float feedbackDisplayTime = 2.0f;
+    private float feedbackCounter = 0;
+    private bool showWrongAnswerFeedback;
+    private bool showRightAnswerFeedback;
+
     private int tutorialCount = 0;
     //Placeholder empty GameObjects in the scene
     [SerializeField]
@@ -20,13 +29,15 @@ public class CardManager : MonoBehaviour
     public int selectedCard;
     private int correctCard;
     private int guessCount = 0;
-    public Canvas cardCanvas;
+
     public Canvas endGameCanvas;
     public Text resultText;
     private int score = 0;
 
     void Awake()
     {
+        correctImage.SetActive(false);
+        failImage.SetActive(false);
         //Ensures that only one instance of CardManager exists
         if (instance != null)
         {
@@ -38,6 +49,41 @@ public class CardManager : MonoBehaviour
         DontDestroyOnLoad(this);
         AssignSprites();
     }
+    private void Update()
+    {
+        if (showWrongAnswerFeedback)
+        {
+
+            if (feedbackCounter < feedbackDisplayTime)
+            {
+                failImage.SetActive(true);
+                print(feedbackCounter);
+            }
+            else
+            {
+                failImage.SetActive(false);
+                showRightAnswerFeedback = false;
+
+            }
+            feedbackCounter += Time.deltaTime;
+        }
+        if (showRightAnswerFeedback)
+        {
+            if (feedbackCounter < feedbackDisplayTime)
+            {
+                correctImage.SetActive(true);
+            }
+            else
+            {
+                correctImage.SetActive(false);
+                showRightAnswerFeedback = false;
+
+            }
+            feedbackCounter += Time.deltaTime;
+
+        }
+    }
+
 
     public void ResetGame() {
         guessCount = 0;
@@ -55,6 +101,7 @@ public class CardManager : MonoBehaviour
             FindObjectOfType<SoundManager>().Play("correctAnswer");
             score += 50;
             scoreDisplay.text = score.ToString();
+            showRightAnswerFeedback = true;
         }
         else
         {
@@ -62,8 +109,9 @@ public class CardManager : MonoBehaviour
             FindObjectOfType<SoundManager>().Play("incorrectAnswer");
             score -= 20;
             scoreDisplay.text = score.ToString();
+            showWrongAnswerFeedback = true;
         }
-        
+
         //after checking for score, move to end round
         EndRound();
     }
@@ -71,7 +119,7 @@ public class CardManager : MonoBehaviour
     private void EndRound()
     {
         // end round is display answer result and feedback to player
-        if(guessCount < 3) {
+        if (guessCount < 3) {
             // start the next round
             AssignSprites();
         } else {
@@ -79,40 +127,61 @@ public class CardManager : MonoBehaviour
         }
     }
 
-    private void EndGame() {
+    private void EndGame()
+    {
         cardCanvas.gameObject.SetActive(false);
         endGameCanvas.gameObject.SetActive(true);
-        resultText.text = "Hyvaa!!!! Sinulla on "+ score.ToString()+" pisteet!!";
+        resultText.text = "Hyvaa!!!! Sinulla on " + score.ToString() + " pisteet!!";
+
+        //public void CheckAnswer() {
+        //    //check if answer is correct, if correct, add 50 to score, if incorrect minus 50 from score
+        //    if (selectedCard == correctCard + 1) {
+        //        Debug.Log("correct");
+        //        showRightAnswerFeedback = true;
+        //        //correctImage.SetActive(true);
+        //        scoreDisplay.text = (Int32.Parse(scoreDisplay.text) + 50).ToString();
+
+        //    } else {
+        //        //failImage.SetActive(true);
+        //        showWrongAnswerFeedback = true;
+        //        Debug.Log("incorrect");
+        //        scoreDisplay.text = (Int32.Parse(scoreDisplay.text) - 50).ToString();
+        //    }
+        //    AssignSprites();
+
+        //    //after checking for score, move to end round
+        //
     }
 
-    public void AssignSprites()
-    {
-        //Creates a list from Card array for later removal
-        List<Card> activeCards = new List<Card>(cardNames);
-        //clear list for old card name before assign new ones
-        currentCardName.Clear();
-        //Loads a sprite for each placeholder
-        for (int i = 0; i < placeholders.Length; ++i)
+        public void AssignSprites()
         {
-            //Chooses a random number below the Card count
-            int rand = UnityEngine.Random.Range(0, activeCards.Count);
-            //Instantiates a Card at position rand
-            Card randCard = activeCards[rand];
-            //Sets the sprite of placeholder GameObject to the sprite component of randCard
-            placeholders[i].GetComponent<SpriteRenderer>().sprite = randCard.cardImage;
-            //Removes the randCard to prevent duplicate sprites from being selected
-            activeCards.Remove(randCard);
-            //add name of randcard to card name list for display
-            currentCardName.Add(randCard.cardType.ToString());
+            //Creates a list from Card array for later removal
+            List<Card> activeCards = new List<Card>(cardNames);
+            //clear list for old card name before assign new ones
+            currentCardName.Clear();
+            //Loads a sprite for each placeholder
+            for (int i = 0; i < placeholders.Length; ++i)
+            {
+                //Chooses a random number below the Card count
+                int rand = UnityEngine.Random.Range(0, activeCards.Count);
+                //Instantiates a Card at position rand
+                Card randCard = activeCards[rand];
+                //Sets the sprite of placeholder GameObject to the sprite component of randCard
+                placeholders[i].GetComponent<SpriteRenderer>().sprite = randCard.cardImage;
+                //Removes the randCard to prevent duplicate sprites from being selected
+                activeCards.Remove(randCard);
+                //add name of randcard to card name list for display
+                currentCardName.Add(randCard.cardType.ToString());
+            }
+            RandomText();
         }
-        RandomText();
-    }
 
-    private void RandomText()
-    {
-        //Selects a random card name from the 3 cards shown
-        int rand = UnityEngine.Random.Range(0, currentCardName.Count);
-        randomText.text = currentCardName[rand];
-        correctCard = rand;
+
+        private void RandomText()
+        {
+            //Selects a random card name from the 3 cards shown
+            int rand = UnityEngine.Random.Range(0, currentCardName.Count);
+            randomText.text = currentCardName[rand];
+            correctCard = rand;
+        }
     }
-}
