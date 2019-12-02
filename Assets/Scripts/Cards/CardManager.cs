3f5ac5bd11af7d58c0dbe2973f5728e8cb22f7d2
ASD <ngocson.bui0312@gmail.com>
2019-12-02 03:56:13 +0200

    Added calibration values for movement from player in advanced skeleton. Added boolean to enable and disable tutorial screen in tutorial manager.

--- Assets/Scripts/Cards/CardManager.cs
+++ Assets/Scripts/Cards/CardManager.cs
@@ -48,7 +48,7 @@ public class CardManager : MonoBehaviour
         //Set current to CardManager
         instance = this;
         DontDestroyOnLoad(this);
-        AssignSprites();
+        //AssignSprites();
     }
     private void Update()
     {
